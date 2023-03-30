69d7f93ad6ed3e8fc3f87b2f15e6ef22bf4b0100
joaopmo <joaopedro6420@gmail.com>
2023-03-30 15:16:25 -0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import React from 'react';
+
+interface ButtonProps {
+    label: string
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button>
+            {props.label}
+        </button>
+    );
+};
+
+export default Button;
